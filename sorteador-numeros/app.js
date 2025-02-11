b05cbc0511ed68e89587a3341d2e7213eaaa50a0
ggeande <geanvgaldino@gmail.com>
2025-02-10 23:58:48 -0300

    subida sortear numeros

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -1,1 +1,24 @@
-console.log('Hello World!');
+
+
+
+function sortear() {
+    let qtd = document.getElementById('quantidade').value;
+    let min = document.getElementById('de').value;
+    let max = document.getElementById('ate').value;
+    let numeros = [];
+    numeros = sortearNumeros(qtd, min, max);
+    let elemento = document.getElementById('resultado');
+    elemento.innerHTML = numeros
+
+}
+
+function sortearNumeros(quantidadeNumero, numeroMinimo, numeroMaximo) {
+    console.log('qtd(' + quantidadeNumero + ')min(' + numeroMinimo + ')max(' + numeroMaximo+')');
+    let numerosSorteados = [];
+    for (let i = 0; i < quantidadeNumero; i++) {
+        let num = Math.floor(Math.random() * (numeroMaximo - numeroMinimo + 1)) + numeroMinimo;
+        console.log('qtd(' + quantidadeNumero + ')min(' + numeroMinimo + ')max(' + numeroMaximo+') numero sorteado ' + num);
+        numerosSorteados.push(num);
+    }
+    return numerosSorteados;
+}
