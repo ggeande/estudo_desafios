6e134355b8d2e578cd922bd24cf925c39ba72ae9
ggeande <geanvgaldino@gmail.com>
2025-02-12 23:56:50 -0300

    ajuste para add e remove class

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -8,12 +8,14 @@ function alterarStatus(id){
 function trocarStatus(img, bot){
 
     if (bot.textContent == 'Alugar') {
-        bot.className = 'dashboard__item__button dashboard__item__button--return';
-        img.className = 'dashboard__item__img dashboard__item__img--return';
+        img.classList.add('dashboard__item__img--rented');
+        bot.classList.add('dashboard__item__button--return'); 
+        //bot.className = 'dashboard__item__button dashboard__item__button--return';
+        //img.className = 'dashboard__item__img dashboard__item__img--return';
         bot.innerHTML = 'Devolver';
     } else {
-        bot.className = 'dashboard__item__button';
-        img.className = 'dashboard__item__img';
+        img.classList.remove('dashboard__item__img--rented');
+        bot.classList.remove('dashboard__item__button--return');
         bot.innerHTML = 'Alugar';
     }
 }
