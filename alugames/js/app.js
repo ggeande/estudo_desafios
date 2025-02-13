adf827a7a6273f04ff4b49fc96a8f2a03dd38dd4
ggeande <geanvgaldino@gmail.com>
2025-02-13 16:44:22 -0300

    iniciar do carrinho

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -10,9 +10,6 @@ function trocarStatus(img, bot){
     if (bot.textContent == 'Alugar') {
         img.classList.add('dashboard__item__img--rented');
         bot.classList.add('dashboard__item__button--return'); 
-        //bot.className = 'dashboard__item__button dashboard__item__button--return';
-        //img.className = 'dashboard__item__img dashboard__item__img--return';
-        bot.innerHTML = 'Devolver';
     } else {
         img.classList.remove('dashboard__item__img--rented');
         bot.classList.remove('dashboard__item__button--return');
--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,11 +1,30 @@
+
 function adicionar(){
     let produto = document.getElementById('produto').value;
     let quantidade = document.getElementById('quantidade').value;
     let valor = pegarValorProduto(produto);
+    
+    buscarCarrinho()
 
     
 }
 
 function pegarValorProduto(prod) {
-    let valor = prod.split(' - R$')[1];
+    return prod.split(' - R$')[1];
+    
 }    
+
+function buscarCarrinho(){
+    let elementoCarrinho = document.getElementById('lista-produtos');
+    let secoesCarrinho = elementoCarrinho.querySelectorAll('section');
+    let quantidadeSecoes = secoesCarrinho.length;
+
+    console.log('Quantidade de produtos no carrinho:', quantidadeSecoes);
+    console.log('Seções do carrinho:', secoesCarrinho.values);
+    
+    secoesCarrinho.forEach((secao) => {
+        console.log('Seção:', secao);
+        
+    });
+
+}
